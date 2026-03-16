fedef8154bdd11a80821548141d43435f48a06ff
Jakub Hloušek <hlousek.jakuba@gmail.com>
2026-03-16 21:27:43 +0100

    Logger utility improvement

--- src/server/src/utils/logger.ts
+++ src/server/src/utils/logger.ts
@@ -1,16 +1,39 @@
 import { getConfig } from "../config";
 
-export function createLogger(namespace: string) {
-
-return {
-  info: (message: string, ...args: unknown[]) => console.log(`[${namespace}] ${message}`, ...args),
-  error: (message: string, ...args: unknown[]) => console.error(`[${namespace}] ERROR: ${message}`, ...args),
-  warn: (message: string, ...args: unknown[]) => console.warn(`[${namespace}] WARN: ${message}`, ...args),
-  debug: (message: string, ...args: unknown[]) => {
-    if (getConfig().nodeEnv === 'development') {
-      console.log(`[${namespace}] DEBUG: ${message}`, ...args);
-    }
-  },
-};
+export enum LogLevel {
+  DEBUG = 0,
+  INFO = 1,
+  WARN = 2,
+  ERROR = 3,
+  NONE = 4,
 }
 
+export function createLogger(
+  namespace: string,
+  minLevel: LogLevel = LogLevel.INFO,
+) {
+  const shouldLog = (level: LogLevel) => level >= minLevel;
+
+  return {
+    debug: (message: string, ...args: unknown[]) => {
+      if (shouldLog(LogLevel.DEBUG) && getConfig().nodeEnv === "development") {
+        console.log(`[${namespace}] DEBUG: ${message}`, ...args);
+      }
+    },
+    info: (message: string, ...args: unknown[]) => {
+      if (shouldLog(LogLevel.INFO)) {
+        console.log(`[${namespace}] ${message}`, ...args);
+      }
+    },
+    warn: (message: string, ...args: unknown[]) => {
+      if (shouldLog(LogLevel.WARN)) {
+        console.warn(`[${namespace}] WARN: ${message}`, ...args);
+      }
+    },
+    error: (message: string, ...args: unknown[]) => {
+      if (shouldLog(LogLevel.ERROR)) {
+        console.error(`[${namespace}] ERROR: ${message}`, ...args);
+      }
+    },
+  };
+}
