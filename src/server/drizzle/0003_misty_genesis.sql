d333bf01216c289044b5521eb19e44a52916ec0a
Jakub Hloušek <hlousek.jakuba@gmail.com>
2026-03-20 23:48:19 +0100

    Removed old migrations

--- src/server/drizzle/0003_misty_genesis.sql
+++
@@ -1,2 +0,0 @@
-CREATE TYPE "public"."time_basis" AS ENUM('synced', 'estimated', 'relative');--> statement-breakpoint
-ALTER TABLE "raw_scans" ADD COLUMN "time_basis" time_basis DEFAULT 'synced' NOT NULL;
